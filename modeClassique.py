ceb57950099a209d519aec80a02b7c6ea7d89548
BammezPaul <bammezpaul@gmail.com>
2021-09-08 09:52:00 +0200

    modes + tests

--- modeClassique.py
+++ modeClassique.py
@@ -2,11 +2,13 @@ import time
 
 if distance1<20:
     Label_score_rouge = Label_score_rouge + 1
+    print(Label_score_rouge)
 
 if distance2<20:
     Label_score_bleu = Label_score_bleu + 1
+    print(Label_score_bleu)
 
---------------""" mode chrono """--------------
+----------------""" mode chrono """-----------
 debut = time.time()
 time.sleep(10)
 
@@ -16,35 +18,41 @@ temps = fin - debut
 
 print(temps)
 
-while temps pas fini 
+while temps < 20:
     if distance1<20:
         Label_score_rouge = Label_score_rouge + 1
+        print(Label_score_rouge)
 
     if distance2<20:
         Label_score_bleu = Label_score_bleu + 1
+        print(Label_score_bleu)
 
 --------------""" mode chrono plus temps """--------------
 
 
-while temps pas fini 
+while temps < 20: 
     valeurbut = 1
     """quand une minute est passée : valeurbut = valeurbut + 1 """
         if distance1<20:
             Label_score_rouge = Label_score_rouge + valeurbut
+            print(Label_score_rouge)
 
         if distance2<20:
             Label_score_bleu = Label_score_bleu + valeurbut
+            print(Label_score_bleu)
 
 --------------""" mode chrono plus but """--------------
 
-while temps pas fini
+while temps < 20:
     valeurbut = 1
     if distance1<20:
         valeurbut = valeurbut + 1
         Label_score_rouge = Label_score_rouge + valeurbut
+        print(Label_score_rouge)
 
     if distance2<20:
         valeurbut = valeurbut + 1
         Label_score_bleu = Label_score_bleu + valeurbut
+        print(Label_score_bleu)
 
     
