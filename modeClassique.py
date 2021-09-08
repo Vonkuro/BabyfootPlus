1d629116e0cce4bca21d430b81a42d1b9de88600
BammezPaul <bammezpaul@gmail.com>
2021-09-08 09:21:44 +0200

    modes

--- modeClassique.py
+++ modeClassique.py
@@ -6,19 +6,19 @@ if distance1<20:
 if distance2<20:
     Label_score_bleu = Label_score_bleu + 1
 
-""" mode chrono """
+--------------""" mode chrono """--------------
 
-while temps pas fini -------------------------------
+while temps pas fini 
     if distance1<20:
         Label_score_rouge = Label_score_rouge + 1
 
     if distance2<20:
         Label_score_bleu = Label_score_bleu + 1
 
-""" mode chrono plus temps """
+--------------""" mode chrono plus temps """--------------
 
 
-while temps pas fini -------------------------------
+while temps pas fini 
     valeurbut = 1
     """quand une minute est passée : valeurbut = valeurbut + 1 """
         if distance1<20:
@@ -27,9 +27,9 @@ while temps pas fini -------------------------------
         if distance2<20:
             Label_score_bleu = Label_score_bleu + valeurbut
 
-""" mode chrono plus but """
+--------------""" mode chrono plus but """--------------
 
-while temps pas fini -------------------------------
+while temps pas fini
     valeurbut = 1
     if distance1<20:
         valeurbut = valeurbut + 1
@@ -38,3 +38,5 @@ while temps pas fini -------------------------------
     if distance2<20:
         valeurbut = valeurbut + 1
         Label_score_bleu = Label_score_bleu + valeurbut
+
+    
