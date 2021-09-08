236baceb0360de6ce9bc29f97efa084b180cce49
Vonkuro <vonkuro@gmail.com>
2021-09-08 11:50:32 +0200

    Bricolage des textes et des boutons, preuve

--- Page_Choix.py
+++ Page_Choix.py
@@ -1,24 +1,22 @@
 import tkinter as tk
+from tkinter.constants import OUTSIDE
 from tkinter.ttk import *
 from PIL import ImageTk, Image 
 
 class Menu():
     def __init__(self):
-        """"
-        #Définition de la fenêtre
-        self.page = tk.Tk()
-        
-        #Paramétrage de la fenêtre
-        self.page.geometry('1920x1080')
-        self.page.resizable(width=0, height=0)
-        """     
         #Définition des Frames
         self.page = tk.Frame()
         self.titre = tk.Frame(master= self.page)
         self.trait = tk.Frame(master= self.page)
         self.corps = tk.Frame(master= self.page)
         self.boutons = tk.Frame(master= self.corps)
+        self.intervale1 = tk.Frame(master= self.boutons)
+        self.intervale2 = tk.Frame(master= self.boutons)
+        self.intervale3 = tk.Frame(master= self.boutons)
+        self.intervale4 = tk.Frame(master= self.boutons)
         self.textes = tk.Frame(master= self.corps)
+        self.intervale_texte = tk.Frame(master= self.textes)
 
         #Paramétrage des Frames
         self.titre.configure(width=1920, height=200, bg="#000000")
@@ -30,12 +28,18 @@ class Menu():
         self.corps.configure(width=1920, height=880, bg="#818181")
         self.corps.pack_propagate(0)
 
+        self.textes.configure(bg="#818181")
+
+        self.intervale_texte.configure(width=613.56, bg="#818181")
+        self.intervale_texte.pack_propagate(0)
+
         #Placement des Frames
         self.titre.pack()
         self.trait.pack()
         self.corps.pack()
-        self.textes.pack()
-        self.boutons.pack()
+        self.textes.place(x=0, y=55)
+        self.intervale_texte.grid(row=0, column=0)
+        self.boutons.place(x=0, y=205)
 
         #Définition des Boutons
         self.Bouton_classique = tk.Button(master= self.boutons)
@@ -46,10 +50,12 @@ class Menu():
         #Paramétrage des Boutons
             #Images
         photo_classique = Image.open(".\Images\Images_Renommee\Mode_Babyfoot.png")
-        
+        taille = (300 , 400)
+        photo_classique = photo_classique.resize(taille)
+
         self.photoImage_classique = ImageTk.PhotoImage(photo_classique)
         
-        self.Bouton_classique.configure(image= self.photoImage_classique, bg= "#4f4f4f")
+        self.Bouton_classique.configure(image= self.photoImage_classique, bg= "#4f4f4f") 
 
         #Définition des Labels
         self.Label_titre = tk.Label(master= self.titre)
@@ -61,14 +67,13 @@ class Menu():
 
         #Placement des Wigets
         self.Label_titre.place(x=495.5, y=65.12)
-        self.Label_bonjour.place(x=613.4, y=270)
-        self.Bouton_classique.grid(row=0, column=0)
-        self.Bouton_chrono.grid(row=0, column=1)
-        self.Bouton_chrono_plus_temps.grid(row=0, column=2)
-        self.Bouton_chrono_plus_but.grid(row=0, column=3)
+        self.Label_bonjour.grid(row=0, column=1)
+        self.Bouton_classique.grid(row=0, column=1)
+        self.Bouton_chrono.grid(row=0, column=3)
+        self.Bouton_chrono_plus_temps.grid(row=0, column=5)
+        self.Bouton_chrono_plus_but.grid(row=0, column=7)
 
-        #Placement de la fenêtre
-        #self.page.mainloop()
+        
 
     def affiche(self):
         self.page.pack()
@@ -77,8 +82,4 @@ class Menu():
     def cache(self):
         self.page.pack_forget()
 
-    def _from_rgb(rgb):
-    #translates an rgb tuple of int to a tkinter friendly color code
-    
-        return "#%02x%02x%02x" % rgb   
 
