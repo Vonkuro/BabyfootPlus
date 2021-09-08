9f473947442e829c61710080b560a64a8eb7599a
BammezPaul <bammezpaul@gmail.com>
2021-09-08 15:50:56 +0200

    je sais pas

--- modeJeu.py
+++ modeJeu.py
@@ -18,23 +18,26 @@ def ModeClassique(enJeu):
 
 
 ----------------""" mode chrono """-----------
+
+def ModeChrono(enJeu):
+    score_rouge = 0
+    score_bleu = 0
+
 debut = time.time()
-time.sleep(10)
 
-fin = time.time()
-
-temps = fin - debut
-
+temps = time.time() - debut
 print(temps)
 
 while temps < 20:
-    if distance1<20:
-        Label_score_rouge = Label_score_rouge + 1
-        print(Label_score_rouge)
+    if but() == 1:
+        score_rouge = score_rouge + 1
+        print("Score rouge :" +str(score_rouge))
+        time.sleep(1)
 
-    if distance2<20:
+    if but() ==2:
         Label_score_bleu = Label_score_bleu + 1
-        print(Label_score_bleu)
+        print("Score bleu :" +str(score_bleu))
+        time.sleep(1)
 
 --------------""" mode chrono plus temps """--------------
 
